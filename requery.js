d4607bcd8d25a08fc559b894128ab5427e9da512
Omor Faruk <farukvai850@gmail.com>
2021-01-11 00:37:16 +0600

    jquery

---
+++ requery.js
@@ -0,0 +1,5 @@
+jQuery(document).ready(function(){
+         jQuery("span.wc-block-components-checkout-step__heading-content>a").click(function(){
+           jQuery(this).attr("href", "https://incomechefs.com/my-account/");
+         });
+       });
